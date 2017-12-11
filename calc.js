779673c8d5cc41e2b8c96035306bb91628eacf53
dawidBielecki <dawid.bielecki01@gmail.com>
2017-12-11 16:57:33 +0100

    end of first calculator

--- calc.js
+++ calc.js
@@ -1,3 +1,50 @@
 'use strict';
+var numField1 = document.getElementById('numField1');
+var numField2 = document.getElementById('numField2');
+var resultField = document.getElementById('resultField');
+var form =document.getElementById('calculateFunction');
+
+form.addEventListener('submit', function(event){
+    
+if (!numField1.value || !numField2.value) {
+    alert("Uzupełnij puste pola!")
+}
+
+else {
+    var x = parseFloat(numField1.value);
+    var y = parseFloat(numField2.value);
+    
+    var result = x / y;
+    var percent = result * 100;
+    
+    resultField.value = percent;
+    event.preventDefault();
+}
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
