543cbf3936dfe043b029259645f1557612e7503f
dawidBielecki <dawid.bielecki01@gmail.com>
2017-12-14 17:00:14 +0100

    end of second calc

--- calc.js
+++ calc.js
@@ -1,8 +1,10 @@
 'use strict';
+// Pierszwy kalkulator
+
 var numField1 = document.getElementById('numField1');
 var numField2 = document.getElementById('numField2');
 var resultField = document.getElementById('resultField');
-var form =document.getElementById('calculateFunction');
+var form = document.getElementById('calculateFunction');
 
 form.addEventListener('submit', function(event){
     
@@ -22,12 +24,30 @@ else {
 }
 });
 
+// drugi kalkulator - Oblicz % z liczby
 
+var numField1v2 = document.getElementById('numField1v2');
+var numField2v2 = document.getElementById('numField2v2');
+var resultFieldv2 = document.getElementById('resultFieldv2');
+var form = document.getElementById('percentOfNumber');
 
+form.addEventListener('submit', function(stop){
 
+if (!numField1v2.value || !numField2v2.value){
+    alert("Uzupełnij brakujące pola");
+}
 
-
-
+else {
+    var x = parseFloat(numField1v2.value);
+    var y = parseFloat(numField2v2.value);
+    
+    var result1 = x / 100;
+    var realResult = result1 * y;
+    
+    resultFieldv2.value = realResult;
+    stop.preventDefault();
+}
+});
 
 
 
